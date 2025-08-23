0b769c7159c7dc3499cbb2475686ad438496e9a1
msaddique2010 <saddiquem2010@gmail.com>
2025-08-23 15:07:39 +0500

    Ruby on Rails last commit

--- db/schema.rb
+++ db/schema.rb
@@ -51,10 +51,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_11_130504) do
   create_table "bugs", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.text "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "project_id", null: false
-    t.string "priority"
     t.integer "user_id"
     t.string "bug_type"
     t.string "status"
